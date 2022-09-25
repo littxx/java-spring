1b3629273594331943280cef9c9963320b6ea5d8
EdenMaisUmDia <ederlanbrito@gmail.com>
2022-09-25 03:41:06 -0300

    ok

---
+++ src/main/java/com/eden/curse/CursinApplication.java
@@ -0,0 +1,13 @@
+package com.eden.curse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eden/curse/CursinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eden.curse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
